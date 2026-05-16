a391e5a9fb31aa128098a58295362187a05dd31b
tala-kha <s12324200@stu.najah.edu>
2026-05-16 13:40:25 +0300

    add firebase request progress logic

--- app/provider/[id].tsx
+++ app/provider/[id].tsx
@@ -15,12 +15,15 @@ const RequestDetails = () => {
   const [requestDetails, setRequestDetails] = useState<any>(null);
   const [userDetails, setUserDetails] = useState<any>(null);
   const [loading, setLoading] = useState(true);
+
   const { id } = useLocalSearchParams();
 
   const fetchData = async () => {
     try {
       setLoading(true);
+
       const docId = Array.isArray(id) ? id[0] : id;
+
       if (!docId || docId === "[id]") return;
 
       const requestRef = doc(db, "requests", docId);
@@ -28,7 +31,11 @@ const RequestDetails = () => {
 
       if (requestSnap.exists()) {
         const requestData = requestSnap.data();
-        setRequestDetails({ id: requestSnap.id, ...requestData });
+
+        setRequestDetails({
+          id: requestSnap.id,
+          ...requestData,
+        });
 
         if (requestData.userUID) {
           const userRef = doc(db, "users", requestData.userUID);
@@ -38,7 +45,7 @@ const RequestDetails = () => {
             setUserDetails(userSnap.data());
           } else {
             console.log(
-              `Path: users/${requestData.userUID} does not exist in Firestore.`,
+              `Path: users/${requestData.userUID} does not exist in Firestore.`
             );
           }
         }
@@ -54,14 +61,15 @@ const RequestDetails = () => {
     fetchData();
   }, [id]);
 
-  if (loading)
-    return (
-      <ActivityIndicator size="large" color="#FF8C00" style={styles.loader} />
-    );
-
   const handleStatusUpdate = async (newStatus: string) => {
     try {
       const docId = Array.isArray(id) ? id[0] : id;
+
+      if (!docId || docId === "[id]") {
+        alert("No request selected.");
+        return;
+      }
+
       const requestRef = doc(db, "requests", docId);
 
       await updateDoc(requestRef, {
@@ -69,9 +77,12 @@ const RequestDetails = () => {
       });
 
       if (newStatus === "accepted") {
-      router.push({
-          pathname: "/shared/request-progress",
-          params: { mode: "provider" }
+        router.push({
+          pathname: "/shared/request-progress" as any,
+          params: {
+            requestId: docId,
+            mode: "provider",
+          },
         });
       } else {
         router.back();
@@ -81,14 +92,23 @@ const RequestDetails = () => {
       alert("Failed to update status. Please try again.");
     }
   };
+
+  if (loading) {
+    return (
+      <ActivityIndicator size="large" color="#FF8C00" style={styles.loader} />
+    );
+  }
+
   return (
     <ScrollView style={styles.screen}>
       {requestDetails ? (
         <View style={styles.container}>
           <Text style={styles.title}>Request Details</Text>
+
           {userDetails ? (
             <View style={styles.detailRow}>
               <Text style={styles.label}>Name</Text>
+
               <View style={{ paddingLeft: 80 }}>
                 <Text style={styles.value}>
                   {userDetails?.fullName || "Not Set"}
@@ -102,30 +122,38 @@ const RequestDetails = () => {
               </Text>
             </View>
           )}
+
           <View style={styles.detailRow}>
             <Text style={styles.label}>Status</Text>
+
             <View style={{ paddingLeft: 75 }}>
               <Text style={styles.value}>{requestDetails.status}</Text>
             </View>
           </View>
+
           <View style={styles.detailRow}>
             <Text style={styles.label}>Service type</Text>
+
             <View style={{ paddingLeft: 30 }}>
               <Text style={styles.value}>{requestDetails.serviceType}</Text>
             </View>
           </View>
+
           {requestDetails.serviceType === "Fuel Delivery" && (
             <View>
               <View style={styles.detailRow}>
                 <Text style={styles.label}>Fuel Type</Text>
+
                 <View style={{ paddingLeft: 50 }}>
                   <Text style={styles.value}>
                     {requestDetails.details?.fuelType}
                   </Text>
                 </View>
               </View>
+
               <View style={styles.detailRow}>
                 <Text style={styles.label}>Fuel Quantity</Text>
+
                 <View style={{ paddingLeft: 25 }}>
                   <Text style={styles.value}>
                     {requestDetails.details?.quantity}
@@ -134,9 +162,11 @@ const RequestDetails = () => {
               </View>
             </View>
           )}
+
           {requestDetails.serviceType === "Tow Truck" && (
             <View style={styles.detailRow}>
               <Text style={styles.label}>Truck Size</Text>
+
               <View style={{ paddingLeft: 30 }}>
                 <Text style={styles.value}>
                   {requestDetails.details?.vehicleSize}
@@ -144,26 +174,32 @@ const RequestDetails = () => {
               </View>
             </View>
           )}
+
           {requestDetails.serviceType === "Tire Service" && (
             <View>
               <View style={styles.detailRow}>
                 <Text style={styles.label}>Vehicle Model</Text>
+
                 <View style={{ paddingLeft: 20 }}>
                   <Text style={styles.value}>
                     {requestDetails.details?.vehicleModel}
                   </Text>
                 </View>
               </View>
+
               <View style={styles.detailRow}>
                 <Text style={styles.label}>Tire Type</Text>
+
                 <View style={{ paddingLeft: 30 }}>
                   <Text style={styles.value}>
                     {requestDetails.details?.tireType}
                   </Text>
                 </View>
               </View>
+
               <View style={styles.detailRow}>
                 <Text style={styles.label}>Tire Numbers</Text>
+
                 <View style={{ paddingLeft: 30 }}>
                   <Text style={styles.value}>
                     {requestDetails.details?.tireCount}
@@ -172,63 +208,60 @@ const RequestDetails = () => {
               </View>
             </View>
           )}
+
           {requestDetails.serviceType === "Jump Start" && (
             <View style={styles.detailRow}>
               <Text style={styles.label}>Immediate assistance request</Text>
             </View>
           )}
+
           {requestDetails.serviceType === "On-site Mechanic" && (
             <View style={styles.detailRow}>
               <Text style={styles.label}>Immediate assistance request</Text>
             </View>
           )}
+
           <View style={styles.detailRow}>
             <Text style={styles.label}>Address</Text>
-            <View style={{ paddingLeft: 60 }}></View>
-            <Text style={[styles.value]}>{requestDetails.address}</Text>
+
+            <View style={{ paddingLeft: 60 }} />
+
+            <Text style={styles.value}>{requestDetails.address}</Text>
           </View>
+
           <View style={styles.detailRow}>
             <Text style={styles.label}>Phone</Text>
-            <View style={{ paddingLeft: 70 }}></View>
-            <Text style={[styles.value]}>{userDetails?.phoneNumber}</Text>
+
+            <View style={{ paddingLeft: 70 }} />
+
+            <Text style={styles.value}>{userDetails?.phoneNumber}</Text>
           </View>
+
           <View style={styles.detailRow}>
             <Text style={styles.label}>Created At</Text>
-            <View style={{ paddingLeft: 43 }}></View>
+
+            <View style={{ paddingLeft: 43 }} />
+
             <Text style={styles.value}>
               {requestDetails.createdAt?.toDate
                 ? requestDetails.createdAt.toDate().toLocaleString()
                 : "No Date"}
             </Text>
           </View>
+
           <View style={{ marginTop: 150 }}>
             <TouchableOpacity
               onPress={() => handleStatusUpdate("accepted")}
               style={styles.accept}
             >
-              <Text
-                style={{
-                  color: "white",
-                  alignSelf: "center",
-                  fontWeight: "bold",
-                }}
-              >
-                Accept Request
-              </Text>
+              <Text style={styles.buttonText}>Accept Request</Text>
             </TouchableOpacity>
+
             <TouchableOpacity
               onPress={() => handleStatusUpdate("rejected")}
               style={styles.reject}
             >
-              <Text
-                style={{
-                  color: "white",
-                  alignSelf: "center",
-                  fontWeight: "bold",
-                }}
-              >
-                RejectRequest
-              </Text>
+              <Text style={styles.buttonText}>Reject Request</Text>
             </TouchableOpacity>
           </View>
         </View>
@@ -240,9 +273,20 @@ const RequestDetails = () => {
 };
 
 const styles = StyleSheet.create({
-  screen: { flex: 1, backgroundColor: "#fff" },
-  container: { padding: 20 },
-  loader: { flex: 1, justifyContent: "center" },
+  screen: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+
+  container: {
+    padding: 20,
+  },
+
+  loader: {
+    flex: 1,
+    justifyContent: "center",
+  },
+
   title: {
     marginTop: 50,
     fontSize: 24,
@@ -251,6 +295,7 @@ const styles = StyleSheet.create({
     color: "#333",
     alignSelf: "center",
   },
+
   detailRow: {
     flexDirection: "row",
     alignItems: "center",
@@ -258,11 +303,13 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderBottomColor: "#eee",
   },
+
   label: {
     fontSize: 16,
     fontWeight: "600",
     color: "#666",
   },
+
   value: {
     fontSize: 16,
     fontWeight: "400",
@@ -273,18 +320,36 @@ const styles = StyleSheet.create({
   card: {
     padding: 15,
   },
-  detailText: { fontSize: 16, color: "#444", marginBottom: 5 },
-  errorText: { color: "red", textAlign: "center", marginTop: 20 },
+
+  detailText: {
+    fontSize: 16,
+    color: "#444",
+    marginBottom: 5,
+  },
+
+  errorText: {
+    color: "red",
+    textAlign: "center",
+    marginTop: 20,
+  },
+
   accept: {
     padding: 20,
     backgroundColor: "#FF8C00",
     borderRadius: 15,
   },
+
   reject: {
     padding: 20,
     marginTop: 20,
     backgroundColor: "#cecdcb",
     borderRadius: 15,
+  },
+
+  buttonText: {
+    color: "white",
+    alignSelf: "center",
+    fontWeight: "bold",
   },
 });
 
